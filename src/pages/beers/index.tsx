11219b0f8d8237c2359e75e7e665e41dece2b56d
boyan damyanov <boyan@boyans-MacBook-Air.local>
2023-03-08 15:07:11 +0200

    ordered imports alphabetically and implemented && conditional rendering

--- src/pages/beers/index.tsx
+++ src/pages/beers/index.tsx
@@ -2,7 +2,14 @@ import Image from 'next/image';
 import axios from 'axios';
 import { get } from 'lodash';
 import {
-  CardContent, CardActions, Button, Grid, CardMedia, Card, Typography, Box, TextField,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
 } from '@mui/material';
 
 import Beer from '@/types';
@@ -29,44 +36,43 @@ export async function getServerSideProps() {
 }
 
 const Beers = ({ data } : Beer) => (
-  <Box sx={{ color: 'primary.main', alignContent: 'center' }}>
+  <Box sx={{ color: 'primary.main', alignContent: 'center', padding: '4rem' }}>
     <Typography variant="h3" align="center">Beers</Typography>
     <TextField sx={{ color: 'white' }} id="outlined-basic" label="Outlined" variant="outlined" />
-    {data.length === 0 ? <Typography>No Beers</Typography> : (
-      <Grid
-        alignItems="center"
-        justifyContent="center"
-        container
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        spacing={{ xs: 2, md: 4 }}
-      >
-        {data.map((beer: Beer) => (
-          <Grid item key={beer.id} xs={12} sm={6} md={4}>
-            <Card
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-            >
-              <Image loader={() => beer.image_url} width={50} height={150} src={beer.image_url} alt="beer" />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h4" component="h2">
-                  {beer.name}
-                </Typography>
-                <Typography variant="h6">
-                  {beer.tagline}
-                </Typography>
-                <Typography variant="h6">
-                  {beer.abv}
-                  {' '}
-                  %
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">View</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    )}
+    {data.length === 0 && <Typography>No Beers</Typography>}
+    <Grid
+      alignItems="center"
+      justifyContent="center"
+      container
+      columns={{ xs: 4, sm: 8, md: 12 }}
+      spacing={{ xs: 2, md: 4 }}
+    >
+      {data.map((beer: Beer) => (
+        <Grid item key={beer.id} xs={12} sm={6} md={4}>
+          <Card
+            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+          >
+            <Image loader={() => beer.image_url} width={50} height={150} src={beer.image_url} alt="beer" />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h4" component="h2">
+                {beer.name}
+              </Typography>
+              <Typography variant="h6">
+                {beer.tagline}
+              </Typography>
+              <Typography variant="h6">
+                {beer.abv}
+                {' '}
+                %
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   </Box>
 );
 export default Beers;
