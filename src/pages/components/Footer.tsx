12812d3a5e7aeb393d546b39fe5996ddc6d4ea6b
boyan damyanov <boyan@boyans-MacBook-Air.local>
2023-03-09 15:14:50 +0200

    styling

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,20 +1,34 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const Footer = () => (
-  <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-    <Typography variant="h6" align="center" gutterBottom>
-      Footer
-    </Typography>
-    <Typography
-      variant="subtitle1"
-      align="center"
-      color="text.secondary"
-      component="p"
+  <CssBaseline>
+    <Box
+      sx={{
+        bgcolor: 'secondary.main',
+        p: 3,
+        position: 'relative',
+        left: 0,
+        bottom: 0,
+        right: 0,
+        width: '100%',
+      }}
+      component="footer"
     >
-      Copyright
-    </Typography>
-  </Box>
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="text.secondary"
+        component="p"
+      >
+        Copyright
+      </Typography>
+    </Box>
+  </CssBaseline>
 );
 
 export default Footer;
