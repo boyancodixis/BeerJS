826b8c0416d69e8391e0396816774044ad98cc68
boyan damyanov <boyan@boyans-MacBook-Air.local>
2023-03-10 10:55:34 +0200

    grid/table view improved

--- src/pages/beers/index.tsx
+++ src/pages/beers/index.tsx
@@ -1,19 +1,15 @@
 import { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
 import axios from 'axios';
 import { get } from 'lodash';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import type { Beer } from '@/types';
 import GridView from '../components/GridView';
 import TableView from '../components/TableView';
+import ViewSwitch from '../components/ViewSwitch';
 
 export async function getServerSideProps() {
   let beerData = [];
@@ -33,20 +29,12 @@ export async function getServerSideProps() {
 
   };
 }
+
 const Beers = ({ data } : Beer) => {
   const [tableView, setTableView] = useState(false);
-  const [gridView, setGridView] = useState(true);
 
   function triggerTableView() {
     setTableView(!tableView);
-  }
-
-  function triggerGridView() {
-    setGridView(!gridView);
-  }
-
-  if (tableView === false && gridView === false) {
-    setGridView(true);
   }
 
   return (
@@ -69,11 +57,18 @@ const Beers = ({ data } : Beer) => {
         <Typography variant="h5">
           Choose a view
         </Typography>
-        <Button variant="contained" onClick={() => triggerGridView()}>Grid</Button>
-        <Button variant="contained" onClick={() => triggerTableView()} sx={{ marginTop: '1rem', marginBottom: '1rem' }}>Table</Button>
+        <Box sx={{ display: 'flex' }}>
+          <FormControlLabel
+            onClick={() => triggerTableView()}
+            control={<ViewSwitch sx={{ m: 1 }} />}
+            label="Table View"
+          />
+        </Box>
       </Box>
-      {tableView && <TableView beers={data} />}
-      {gridView && <GridView beers={data} />}
+      {/* {tableView && <TableView beers={data} />}
+      {gridView && <GridView beers={data} />} */}
+
+      {tableView ? <TableView beers={data} /> : <GridView beers={data} />}
       {data.length === 0 && <Typography>No Beers</Typography>}
     </Box>
   );
