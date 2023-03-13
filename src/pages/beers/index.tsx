475c598826aaaf69c90464f5e6e6dd13d42c2edf
boyan damyanov <boyan@boyans-MacBook-Air.local>
2023-03-13 16:42:32 +0200

    search implemented

--- src/pages/beers/index.tsx
+++ src/pages/beers/index.tsx
@@ -3,7 +3,11 @@ import axios from 'axios';
 import { get } from 'lodash';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
 import Pagination from '@mui/material/Pagination';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
@@ -16,6 +20,7 @@ import ViewSwitch from '../../components/ViewSwitch';
 const Beers = ({ data } : Beer[]) => {
   const [view, setView] = useState<ViewType>(BeerView.grid);
   const [page, setPage] = useState(1);
+  const [beerName, setBeerName] = useState<Beer[]>();
   const [beers, setBeers] = useState<Beer[]>();
 
   const getBeers = useCallback(async () => {
@@ -27,6 +32,17 @@ const Beers = ({ data } : Beer[]) => {
       console.error(error);
     }
   }, [page]);
+
+  const getBeersByName = async () => {
+    const beerNameUrl = `https://api.punkapi.com/v2/beers?beer_name=${beerName}`;
+
+    try {
+      const allBeers = await axios.get(beerNameUrl);
+      setBeerName(allBeers.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     getBeers();
@@ -43,25 +59,28 @@ const Beers = ({ data } : Beer[]) => {
       setView(BeerView.grid);
     }
   };
+  console.log(beerName);
 
   return (
     <Box sx={{ color: 'primary.main', alignContent: 'center', padding: '4rem' }}>
       <Typography variant="h3" align="center" fontWeight="bold" sx={{ textDecoration: 'underline' }}>Beers</Typography>
       <Box sx={{ display: 'flex' }}>
-        <TextField
-          sx={{
-            color: 'white',
-            width: '30%',
-            marginBottom: '3rem',
-          }}
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-        />
-        <Button>Search</Button>
+        <FormControl>
+          <TextField
+            sx={{
+              color: 'white',
+              width: '100%',
+            }}
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            onChange={(e) => setBeerName(e.target.value)}
+          />
+          <Button type="submit" onClick={getBeersByName} variant="contained" sx={{ marginTop: '.5rem' }}>Search</Button>
+        </FormControl>
       </Box>
       <Box sx={{ display: 'flex', flexDirection: 'column', float: 'left' }}>
-        <Typography variant="h5" mb={3}>
+        <Typography variant="h5" display="flex" justifyContent="center" mt={5} mb={3}>
           Choose a view
         </Typography>
         <Box sx={{ display: 'flex' }}>
